556a410d0d2108df33db59141a87a84aebef01d8
Imants <imants.kusins@gmail.com>
2015-07-02 13:44:23 +0100

    Started error logging

--- src/Shell/system/System.php
+++ src/Shell/system/System.php
@@ -25,13 +25,34 @@ class System
 
     private function _parseCommand()
     {
-        if (count($this->_command) === 0) {
+
+        if ( ! $this->_commandHasBeenPassed() ) {
             $this->_output("Please pass a command to the CLI.");
         }
+
+        // Verify if the command exists in the engine.
+        if ( ! $this->_commandExists() ) {
+            $this->_output("Command '" . $this->_command[1] . "' doesn't exist.");
+        }
+
 
         return true;
     }
 
+
+
+    private function _commandExists()
+    {
+        return false;
+    }
+
+
+    private function _commandHasBeenPassed()
+    {
+        if (count($this->_command) === 0) return false;
+
+        return true;
+    }
 
 
     private function _output($outputString)
@@ -42,7 +63,7 @@ class System
 
     private function _fail()
     {
-        echo "Such command has not been found!" . PHP_EOL;
+        echo $this->_command[1] . " command has not been found!" . PHP_EOL;
         die();
     }
 
